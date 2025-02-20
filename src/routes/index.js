9c41e3ceb66d589ed3ffeaf8b8d888e6bd04760f
HerminioUnisced <hmacamo@estudantes.unisced.edu.mz>
2025-02-20 09:50:13 +0200

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthRoutes from "./auth.route";
+import AppRoutes from "./app.route";
+
+export default function Routes ()
+{
+    return (
+        <AuthRoutes />
+    );
+}
